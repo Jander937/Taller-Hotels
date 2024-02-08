740b9df897378619cedb528b471fc1c619d4f07e
Jander <janderburitica123@gmail.com>
2024-02-07 20:44:11 -0500

    actualizacion services

--- hotels/src/main/java/com/co/buritica/hotels/models/dtos/HotelDTO.java
+++ hotels/src/main/java/com/co/buritica/hotels/models/dtos/HotelDTO.java
@@ -1,5 +1,7 @@
 package com.co.buritica.hotels.models.dtos;
 
+import com.co.buritica.hotels.models.entities.CitiesEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
--- hotels/src/main/java/com/co/buritica/hotels/models/entities/SuitesTypesEntity.java
+++ hotels/src/main/java/com/co/buritica/hotels/models/entities/SuitesTypesEntity.java
@@ -1,7 +1,6 @@
 package com.co.buritica.hotels.models.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
